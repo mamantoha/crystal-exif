293f821ae49b2097f15ceb3879c20f213e38a2a9
Anton Maminov <anton.linux@gmail.com>
2022-04-22 12:28:04 +0300

    add GPS tags

--- spec/crystal-exif_spec.cr
+++ spec/crystal-exif_spec.cr
@@ -11,5 +11,7 @@ describe Exif do
     hsh["make"].should eq("NIKON")
     hsh["model"].should eq("COOLPIX P6000")
     hsh["user_comment"].should eq("")
+    hsh["gps_latitude"].should eq("43, 28, 2.81400000")
+    hsh["gps_longitude"].should eq("11, 53, 6.45599999")
   end
 end
