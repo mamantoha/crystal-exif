73d1fc8a5015f826ed78e29dabbc547a1ebdd4dc
Anton Maminov <anton.linux@gmail.com>
2026-08-01 16:18:35 +0300

    Show detailed EXIF metadata in web sample

--- samples/web/src/main.cr
+++ samples/web/src/main.cr
@@ -1,6 +1,37 @@
 require "mime"
 require "kemal"
 require "exif"
+require "html"
+
+RAW_PREVIEW_SIZE = 128
+
+record EntryDetails,
+  tag : String,
+  tag_id : Int32,
+  ifd : String,
+  format : String,
+  components : LibC::ULong,
+  size : LibC::UInt,
+  value : String,
+  raw_bytes : String,
+  raw_truncated : Bool
+
+def entry_details(entry : Exif::Entry) : EntryDetails
+  bytes = entry.raw_bytes
+  preview_size = Math.min(bytes.size, RAW_PREVIEW_SIZE)
+
+  EntryDetails.new(
+    tag: entry.tag.to_s.lchop("ExifTag"),
+    tag_id: entry.tag.value,
+    ifd: entry.ifd.to_s.lchop("ExifIfd"),
+    format: entry.format.to_s.lchop("ExifFormat"),
+    components: entry.components,
+    size: entry.size,
+    value: entry.display_value,
+    raw_bytes: bytes[0, preview_size].hexstring,
+    raw_truncated: bytes.size > preview_size,
+  )
+end
 
 get "/" do
   render "src/views/index.ecr"
@@ -16,12 +47,18 @@ post "/upload" do |env|
   encoded = Base64.encode(content)
 
   data = {} of String => String
+  mnote_data = {} of String => String
+  entries = [] of EntryDetails
+  byte_order = nil
   error_message = nil
 
   begin
     file.rewind
     exif = Exif.new(file)
     data = exif.data.transform_keys(&.camelcase)
+    mnote_data = exif.mnote_data
+    entries = exif.entries.map { |entry| entry_details(entry) }
+    byte_order = exif.byte_order.to_s.lchop("ExifByteOrder")
   rescue Exif::Error
     error_message = "This image does not contain readable EXIF metadata."
   end
